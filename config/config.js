b60eb3f84fdd8344ffefbce2f58fbb43944c1cb1
Dtheran <dtheran1.0@gmail.com>
2023-08-27 21:24:38 -0500

    implementando env

---
+++ config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const config = {
+  env: process.env.NODE_ENV || 'dev',
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER || 'dani',
+  dbPassword: process.env.DB_PASSWORD || 'admin1234',
+  dbHost: process.env.DB_HOST || 'localhost',
+  dbName: process.env.DB_NAME || 'my_store',
+  dbPort: process.env.DB_PORT || 5432,
+};
+
+module.exports = { config };
