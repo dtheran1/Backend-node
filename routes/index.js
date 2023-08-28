ca905999a7cd9c56e4bb0ae48be386e1c300bab8
Dtheran <dtheran1.0@gmail.com>
2023-08-28 13:59:34 -0500

    relation 1 to 1

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const productsRouter = require('./products.router');
 const categoriesRouter = require('./categories.router');
 const usersRouter = require('./users.router');
 const orderRouter = require('./orders.router');
+const customerRouter = require('./customers.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productsRouter);
+  router.use('/customers', customerRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
   router.use('/orders', orderRouter);
