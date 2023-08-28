81ad3f57dc7f8dd3ba35e13296f81a71f1fe78c1
Dtheran <dtheran1.0@gmail.com>
2023-08-28 12:54:57 -0500

    wip

--- config/config.js
+++ config/config.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
+  //env: process.env.NODE_ENV,
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER || 'dani',
-  dbPassword: process.env.DB_PASSWORD || 'admin1234',
-  dbHost: process.env.DB_HOST || 'localhost',
-  dbName: process.env.DB_NAME || 'my_store',
-  dbPort: process.env.DB_PORT || 5432,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
 };
 
 module.exports = { config };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const { config } = require('../config/config');
+const { config } = require('./../config/config');
 const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
@@ -16,5 +16,7 @@ const sequelize = new Sequelize(URI, {
 });
 
 setupModels(sequelize);
+
+sequelize.sync();
 
 module.exports = sequelize;
