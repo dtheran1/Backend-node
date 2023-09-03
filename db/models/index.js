ce5a3f14c02841da91211c93f7e1e9e76dfd0ef4
Dtheran <dtheran1.0@gmail.com>
2023-09-02 19:21:44 -0500

    fix Category and product init

--- db/models/index.js
+++ db/models/index.js
@@ -1,13 +1,13 @@
 const { User, UserSchema } = require('./user.model');
 const { Customer, CustomerSchema } = require('./customer.model');
-const { Category } = require('./category.model');
-const { Product } = require('./product.model');
+const { Category, CategorySchema } = require('./category.model');
+const { Product, ProductSchema } = require('./product.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
-  Category.init(Category.config(sequelize));
-  Product.init(Product.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
 
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
